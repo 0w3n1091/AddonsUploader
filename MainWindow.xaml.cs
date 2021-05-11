8b7cb5a500acc2634ec436be6abb30dcd0a24798
ggwarys <grzegorzgwarys@gmail.com>
2021-05-11 21:18:07 +0200

    first commit, UI, button, path on label script

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace AddonsUploader
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PathButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                PathLabel.Content = dialog.FileName;
+
+            }
+        }
+
+        private void InterfaceCheck_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WTFCheck_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
