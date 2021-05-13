5e662e315c9fce54477bf0a9ff12af0bc90dfe51
ggwarys <grzegorzgwarys@gmail.com>
2021-05-13 14:48:24 +0200

    working path button, checkboxes, path label, creating settings file with interface folder

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+
+
 namespace AddonsUploader
 {
     /// <summary>
@@ -21,18 +24,48 @@ namespace AddonsUploader
     public partial class MainWindow : Window
     {
         private CommonOpenFileDialog _dialog;
+        private static readonly string _settingsFolder = "E:\\", _settingsName = "\\settings.txt";
+        private static readonly string _settingsFullPath = _settingsFolder + _settingsName;
+        private string _wtfPath;
+        private string _intPath;
+        private string _wowPath;
+
+        
+
         public MainWindow()
         {
             InitializeComponent();
-            InitializeDialog();
-        }
-        private void InitializeDialog()
-        {
-            _dialog = new CommonOpenFileDialog("Wybierz folder")
+            InitializeElements();
+            if (File.Exists(_settingsFullPath))
             {
-                InitialDirectory = "C:\\",
-                IsFolderPicker = true
-            };
+                _wowPath = File.ReadAllText(_settingsFullPath);
+                _wtfPath = _wowPath + "\\WTF";
+                _intPath = _wowPath + "\\Interface";
+                PathLabel.Content = _wowPath;
+                FolderCheck();
+            }
+        }
+
+        private void InitializeElements()
+        {
+            InterfaceCheck.IsEnabled = false;
+            WTFCheck.IsEnabled = false;
+            if (File.Exists(_settingsFullPath))
+            {
+                _dialog = new CommonOpenFileDialog()
+                {
+                    InitialDirectory = File.ReadAllText(_settingsFullPath),
+                    IsFolderPicker = true,
+                };
+            }
+            else
+            {
+                _dialog = new CommonOpenFileDialog()
+                {
+                    InitialDirectory = "C:\\",
+                    IsFolderPicker = true
+                };
+            }
         }
 
         private void PathButton_Click(object sender, RoutedEventArgs e)
@@ -40,17 +73,44 @@ namespace AddonsUploader
             if (_dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 PathLabel.Content = _dialog.FileName;
+                _wtfPath = _dialog.FileName + "\\WTF";
+                _intPath = _dialog.FileName + "\\Interface";
+                FolderCheck();
+                SettingsSave();
             }
+        }
+
+        private void FolderCheck()
+        {
+            if (Directory.Exists(_intPath))
+            {
+                InterfaceCheck.IsChecked = true;
+            }
+            if (Directory.Exists(_wtfPath))
+            {
+                WTFCheck.IsChecked = true;
+            }
+            else
+            {
+                InterfaceCheck.IsChecked = false;
+                WTFCheck.IsChecked = false;
+            }
+        }
+
+        private void SettingsSave()
+        {
+            File.WriteAllText(_settingsFolder + _settingsName,_dialog.FileName);
         }
 
         private void InterfaceCheck_Checked(object sender, RoutedEventArgs e)
         {
-
+         
         }
 
         private void WTFCheck_Checked(object sender, RoutedEventArgs e)
         {
 
         }
+        
     }
 }
